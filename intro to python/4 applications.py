7a5c4c04cfdf449f2b0a737d142ac1816184b3af
mudasirbashir <mudasir.bhatti358@gmail.com>
2023-08-19 22:15:22 -0700

    applications file added

---
+++ intro to python/4 applications.py
@@ -0,0 +1,9 @@
+# we can use it in
+# 1. making webapps(django)
+# 2. making desktop apps(tinker library) 
+# 3. machine learning 
+# 4. game development
+# 5. web scrapping 
+# 6. scientific calculations 
+# 7. ai 
+# vlc made with python 
