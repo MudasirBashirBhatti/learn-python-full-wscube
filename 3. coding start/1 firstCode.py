cd1308b70b0d7fa44518acd7a65e3b4d03e65d2a
mudasirbashir <mudasir.bhatti358@gmail.com>
2023-08-19 23:10:12 -0700

    coding start file added

---
+++ 3. coding start/1 firstCode.py
@@ -0,0 +1,6 @@
+x = 5
+print(x)
+print("Mudasir")
+print("Mudasir",1) #inside print we can write multiple coma separated files
+
+# to print code using terminal just go to folder of .py file and write "Python filename.py" 
---
+++ 3. coding start/python from notepad.py
@@ -0,0 +1,3 @@
+# 1. create folder named "program" on desktop > create file named "file.py" > write code in "file.py" > run cmd > cd desktop > cd program > python file.py
+
+# ***********the code will start to run**********
