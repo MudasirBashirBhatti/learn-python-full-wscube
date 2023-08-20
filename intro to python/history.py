c4150f967e9f14c647d6ea522e20092584f6f322
mudasirbashir <mudasir.bhatti358@gmail.com>
2023-08-19 21:57:15 -0700

    python history added

--- intro to python/history.py
+++ intro to python/history.py
@@ -1,1 +1,5 @@
 # python was introduced by Guido Van Rossum in 1989 
+# first version of python released in 1991 
+# in 1994 python 1.0 was released with some new features introduced (map, filter and lambda)
+# python 2.X also add some more features like (comprehension and garbage collection)
+# current version is 3.X released in 2008
