fedc5b36a9d5fd28da97775e7a9aedf9844a7d2b
mudasirbashir <mudasir.bhatti358@gmail.com>
2023-08-19 22:08:03 -0700

    features file uploaded

---
+++ intro to python/3features.py
@@ -0,0 +1,8 @@
+# 1. easy to learn and implement. Open source 
+# 2. broad standard library(i.e numpy for machine learning)
+# 3. cross platform(we can run it on every operating system like linux, mac or windows)
+# 4. it is multi-pardigm language(we can write it structural as well as object oriented way)  
+# 5. high level programming language means we can understand code 
+# 6. it is extendable language means we can combine it with other languages and run 
+# 7. it is expressive programming language the syntex is easily understandable 
+# 8. gui programming support(we can use it for desctop applications)
