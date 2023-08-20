c88de017bf56d33b503f6362a826ddc24d54b410
mudasirbashir <mudasir.bhatti358@gmail.com>
2023-08-19 22:31:15 -0700

    installation process added

---
+++ 2. installation of Python/1. installation process.py
@@ -0,0 +1,7 @@
+# 1. visit https://python.org/
+# 2. go to "downloads" tab and download lattest version for windows 
+# 3. click on downloaded file and install it 
+# 4. must check add to path > custom installation > all should checked(pip is very important which is used to download packeges) > next
+
+# 5. in advanced options check "install for all users"
+# 6. install python direct in c drive (C:\Python38.32) > install
---
+++ 2. installation of Python/2. check python installed.py
@@ -0,0 +1,3 @@
+# 1: run cmd >
+# run this command in cmd > Python
+# it will show the lattest version 
