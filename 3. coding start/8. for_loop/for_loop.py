648a909d4e059b967cdadc6343e2df7238a91fee
mudasirbashir <mudasir.bhatti358@gmail.com>
2023-08-21 09:22:54 -0700

    for loop added

---
+++ 3. coding start/8. for_loop/for_loop.py
@@ -0,0 +1,51 @@
+range(5)
+# above statement means
+# start point 0
+# end point < 5
+# increment +1
+# return 0 1 2 3 4
+print("..........range(5).........")
+for n in range(5):
+    print(n)
+
+
+
+
+
+range(2,5)
+# start point 2
+# end point < 5
+# increment +1
+# return 2 3 4
+print("..........range(2,5).........")
+for n in range(2,5):
+    print(n)
+
+
+
+
+
+range(1,10,2)
+# start point 2
+# end point < 5
+# increment +3
+# return 1 3 5 7 9 
+print("..........range(2,5).........")
+for n in range(1,10, 2):
+    print(n)
+
+
+
+print(".............complex example...........")
+strPoint = int(input("startingPoint: "))
+endPoint = int(input("endingPoint: "))
+inc = int(input("increment by : "))
+
+for n in range(strPoint, endPoint, inc):
+    print(n)
+
+
+
+
+
+        
---
+++ 3. coding start/8. for_loop/for_loop_examples.py
@@ -0,0 +1,6 @@
+
+print(".............table...........")
+tableCount = int(input("startingPoint: "))
+
+for n in range(1,11,1):
+    print(tableCount,"*",n,"=",tableCount*n)
