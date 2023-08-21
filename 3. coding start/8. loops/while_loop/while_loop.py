f8b2f925e12649fa6ca409947bf77a1d16d16601
mudasirbashir <mudasir.bhatti358@gmail.com>
2023-08-21 09:43:08 -0700

    while loop added

---
+++ 3. coding start/8. loops/while_loop/while_loop.py
@@ -0,0 +1,16 @@
+# while loop manage conditions just like if else
+# while loop will run until condition is false.
+
+i=1                     #start
+while i<=5:             #condition
+    i=i+1               #increment or decrement(if not write this it will be infinite loop)
+    print('hello')      #statement
+
+print(i)
+# in while loop i++ or i-- will not work here you can do i+1
+
+# reverse loop 
+a = 5
+while a>=0:
+    a=a-1
+    print('value is',a)
