9efc331fd531268ee733836aab02920036bce138
Mudasir <mudasir.bhatti358@gmail.com>
2026-04-25 07:57:38 +0500

    logs types in python logging module

---
+++ 3. coding start/15_logging/logs.py
@@ -0,0 +1,8 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG) # Set the logging level to INFO to capture info, warning, and error messages
+
+logging.debug("This is a debug message")
+logging.info("This is an info message")
+logging.warning("This is a warning message")
+logging.error("This is an error message")
